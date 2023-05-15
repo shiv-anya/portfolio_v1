e17927fbec9237dcb342498634f645f85f5c2d2d
shiv-anya <sg90883@gmail.com>
2023-05-15 23:56:35 +0530

    added all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from "react";
 import Banner from "./components/Intro/Banner";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <Fragment>
       <Banner />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
     </Fragment>
   );
 }
