5d7afe0e8dcc3fb1734a3d3e2c5f4fcb2b96343b
shiv-anya <sg90883@gmail.com>
2023-05-15 23:55:31 +0530

    changed boxshadow values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "2xl": "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+      },
+    },
   },
   plugins: [],
 };
