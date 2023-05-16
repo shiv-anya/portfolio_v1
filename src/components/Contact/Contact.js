f3f812d8503482e98c52061af6d62febce0f59d6
shiv-anya <sg90883@gmail.com>
2023-05-16 23:44:07 +0530

    added social media links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import ContactCard from "../Intro/ContactsCard";
 
 const Contact = () => {
   return (
@@ -10,15 +11,32 @@ const Contact = () => {
             Talk about open source contribution or for an opportunity. My inbox
             is open for all.
           </p>
-          <p className="text-2xl mb-5">Delhi, India</p>
+          <p className="text-2xl mb-5">
+            <i class="fa-regular fa-location-dot"></i>Delhi, India
+          </p>
           <p className="text-2xl mb-5">Open for opportunities: YES</p>
-          <div>contacts</div>
+          <div className="mt-10">
+            <ContactCard />
+          </div>
         </div>
-        <div>img</div>
+        <div></div>
       </div>
       <div className="h-36 flex justify-center items-center text-xl">
-        <p>Made with love by SG</p>
-        <a href="#about">Up</a>
+        <p className="flex justify-center items-center">
+          Made with{" "}
+          <img
+            className="h-5 w-5 mx-1"
+            alt="heart"
+            src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/72x72/2764.png"
+          />{" "}
+          by SG
+        </p>
+        <a
+          href="#about"
+          className="fixed bottom-10 right-10 bg-purple-800 rounded-lg p-2 flex justify-center items-center"
+        >
+          <i className="fas fa-hand-point-up text-white text-5xl"></i>
+        </a>
       </div>
     </Fragment>
   );
