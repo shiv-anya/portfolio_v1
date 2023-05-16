12cfb0e4e69ef0b28de1ea290ad839fc43e7f22d
shiv-anya <sg90883@gmail.com>
2023-05-17 00:56:25 +0530

    added live and github link for projects

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -3,28 +3,36 @@ import React, { Fragment } from "react";
 const ProjectCard = (props) => {
   return (
     <Fragment>
-      <div className="shadow-2xl h-96 w-80 rounded-lg my-5">
+      <div className="shadow-2xl h-auto w-80 rounded-lg my-5">
         <img
           src={props.src}
           alt="project"
           className="h-44 w-full rounded-t-lg"
         />
-        <div className="flex flex-col items-center p-9">
-          <h2 className="text-xl">{props.name}</h2>
-          <p className="text-xl">{props.timeline}</p>
-          <p className="text-justify text-xl">
-            <ul className="flex">
-              {props.about.map((a) => (
-                <li>{a}</li>
-              ))}
-            </ul>
-          </p>
-          <div>
-            <ul className="flex">
+        <div className="flex flex-col items-center p-5 justify-between">
+          <h2 className="text-2xl py-2 text-center">{props.name}</h2>
+          <p className="text-sm py-2">({props.timeline})</p>
+          <p className="text-center text-xl">{props.about}</p>
+          <div className="py-2">
+            <ul className="flex flex-wrap justify-center">
               {props.stacks.map((s) => (
-                <li>{s}</li>
+                <li className="mr-3 shadow-inner p-1 rounded-sm mt-2">{s}</li>
               ))}
             </ul>
+          </div>
+          <div className="my-5">
+            <a
+              href={props.live}
+              className="text-base uppercase bg-purple-800 text-white py-1 px-3 rounded-lg hover:bg-white hover:text-purple-800 hover:border-2 hover:border-purple-800 mr-5"
+            >
+              Live
+            </a>
+            <a
+              href={props.github}
+              className="text-base uppercase bg-purple-800 text-white py-1 px-3 rounded-lg hover:bg-white hover:text-purple-800 hover:border-2 hover:border-purple-800"
+            >
+              GitHub
+            </a>
           </div>
         </div>
       </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,44 +10,62 @@ const data = [
   {
     name: "BudgetBud",
     timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    about:
+      "BudgetBud is expense tracker with basic CRUD operations. And representation of monthly expenditure and yearly expenditure by charts",
+    stacks: ["React", "Nodejs", "MongoDB", "Express", "Chart.js"],
     src: BudgetBud,
+    live: "https://budgetbud23.netlify.app/",
+    github: "https://github.com/shiv-anya/BudgetBud",
   },
   {
-    name: "BudgetBud",
-    timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    name: "Insta Clone",
+    timeline: "Sept 2021 - Current",
+    about:
+      "Insta clone app that gives you same visual experience. It can create posts. And show newsfeed. Authentication like signup and login available.",
+    stacks: ["React", "Firebase"],
     src: InstaClone,
+    live: "https://insta-clone21.netlify.app/",
+    github: "https://github.com/shiv-anya/insta-clone",
   },
   {
-    name: "BudgetBud",
-    timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    name: "User Module",
+    timeline: "Sept 2022 - Current",
+    about:
+      "User Management Module. To manage your employees data at one place. Having two modes Admin and User. Includes all CRUD ops with authentication using email verification.",
+    stacks: ["React", "Nodejs", "MongoDB", "Express", "OAuth", "JWT"],
     src: UserModule,
+    live: "https://user-module22.netlify.app/",
+    github: "https://github.com/shiv-anya/user_module",
   },
   {
-    name: "BudgetBud",
-    timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    name: "Appetizer",
+    timeline: "May 2022 - Current",
+    about:
+      "Restaurant app. Which is created keeping responsiveness, UI experience and theme in mind.",
+    stacks: ["HTML", "CSS"],
     src: Appetizer,
+    live: "https://appetizer-res.netlify.app/",
+    github: "https://github.com/shiv-anya/restaurant",
   },
   {
-    name: "BudgetBud",
+    name: "Covid Cases Tracker",
     timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    about:
+      "It keep track of covid cases and it's representation countrywise and comparing deaths, total cases and recovered cases by line graph.",
+    stacks: ["React", "Chart.js", "React Leaflet"],
     src: FrontendTask,
+    live: "https://frontend-task23.netlify.app/",
+    github: "https://github.com/shiv-anya/Frontend-task",
   },
   {
-    name: "BudgetBud",
-    timeline: "May 2023 - Current",
-    about: ["lots of stuff"],
-    stacks: ["React", "Nodejs", "MongoDB", "Express"],
+    name: "SpaceX Capsules Record",
+    timeline: "April 2023 - Current",
+    about:
+      "Here you can know about all spaceX capsules and their details if you a hardcore fan. Filter records on basis of name, status and id.",
+    stacks: ["React"],
     src: SpaceX,
+    live: "https://spacex23.netlify.app/",
+    github: "https://github.com/shiv-anya/ShivangiGupta_Frontend-Developer",
   },
 ];
 const Projects = () => {
@@ -63,11 +81,15 @@ const Projects = () => {
               about={p.about}
               stacks={p.stacks}
               src={p.src}
+              live={p.live}
+              github={p.github}
             />
           ))}
         </div>
         <a
           href="https://github.com/shiv-anya"
+          target="_blank"
+          rel="noreferrer"
           className="text-xl uppercase bg-purple-800 text-white py-3 px-5 rounded-lg hover:bg-white hover:text-purple-800 hover:border-2 hover:border-purple-800 mt-5"
         >
           See more
