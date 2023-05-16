3450e4e370edbbac28c17711ebc0623fb3946a31
shiv-anya <sg90883@gmail.com>
2023-05-17 00:57:03 +0530

    added boxshadow styles as inner

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     extend: {
       boxShadow: {
         "2xl": "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+        inner:
+          "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
       },
       fontFamily: {
         monte: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
